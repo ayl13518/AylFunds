916a90e894a70eba567ac781077cd6cd2a63cc45
Ascendens Asia loppy <ayl13518@gmail.com>
2024-10-29 15:31:57 +0800

    trans list grouping

--- app/src/main/java/com/example/newnav/ExpenseListScreen.kt
+++ app/src/main/java/com/example/newnav/ExpenseListScreen.kt
@@ -1,5 +1,6 @@
 package com.example.newnav
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,12 +13,15 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -25,12 +29,15 @@ import androidx.navigation.compose.rememberNavController
 import com.example.newnav.navigation.NavigationBottomBar
 import com.example.newnav.viewmodels.expTransViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ExpListScreen(
     navController: NavHostController = rememberNavController(),
     viewModel: expTransViewModel = viewModel()
 ){
     val state by viewModel.state.collectAsState()
+
+    val transbyDate = state.expTrans.groupBy { it.dateTrans  }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -54,15 +61,17 @@ fun ExpListScreen(
             modifier = Modifier.fillMaxSize(),
             contentPadding = paddingValues
         ) {
-            item {
-                Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
-                Text(text = "Begin")
-            }
-            items(state.expTrans.toList()){ msg ->
-                Text(text = msg.amount.toString())
-            }
-            item {
-                Text(text = "Last")
+            transbyDate.forEach { (initialDate, expsByDate) ->
+                stickyHeader {
+                    Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
+                    Text(text = initialDate,
+                        textAlign = TextAlign.Center,
+                        style = typography.titleMedium,
+                    )
+                }
+                items(expsByDate) { exp ->
+                    Text(text = exp.amount.toString())
+                }
             }
         }
 
